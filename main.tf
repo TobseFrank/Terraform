3b803fac68d15ea09a2345da385e527f222b59b4
Tobias Frank <Tobiasfr1@yahoo.de>
2022-05-10 00:59:04 +0200

    update

--- main.tf
+++ main.tf
@@ -2,48 +2,12 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable "website_bucket_name" {
-  type = string
-}
-
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.website_bucket_name
-  acl    = "public-read"
-
-  policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    { "Sid": "PublicRedForGetBucketObjects",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS":"*"
-      },
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${var.website_bucket_name}/*"
-    }
-  ]
-}
-EOF
-
-  website {
-    index_document = "index.html"
-  }
-}
-
-data "aws_region" "region" {
-
+module "static_website" {
+  source = "./static_website"
+  index_path = "index.html"
+  website_bucket_name = "asdklfjalskdjqwpoitreuj"
 }
 
 output "website_url" {
-  value = "http://${aws_s3_bucket.website_bucket.bucket}.s3-website.${data.aws_region.region.name}.amazonaws.com"
-}
-
-resource "aws_s3_bucket_object" "index_upload" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-
-  source = "index.html"
-
-  content_type = "text/html"
+  value = module.static_website.website_url
 }
---
+++ static_website/variables.tf
@@ -0,0 +1,8 @@
+variable "website_bucket_name" {
+  type = string
+}
+
+variable "index_path" {
+  type = string
+  description = "Path to index.html"
+}
